aa88b6d70dd67ad183d3ce62cc581faf5c69e8e6
Son Mac <mactranthienson@gmail.com>
2022-10-14 02:58:21 -0700

    Create a file of constants

---
+++ constants.py
@@ -0,0 +1,9 @@
+SLOTTIME = 10**-5  # seconds
+SIMULATIONTIME = 10  # seconds
+LAMBDAS = [100, 200, 300, 400, 700]
+DIFS = 4  # slots
+SIFS = 1  # slots
+ACK = 2  # slots
+FRAME = 100  # slots
+MAX_CW = 1024
+MIN_CW = 4
