6114b814fc342596ee38e4bd86fdc5834135d3a2
Son Mac <mactranthienson@gmail.com>
2022-10-17 04:11:21 -0700

    Update constants file

--- constants.py
+++ constants.py
@@ -1,9 +1,10 @@
 SLOTTIME = 10**-5  # seconds
 SIMULATIONTIME = 10  # seconds
-LAMBDAS = [100, 200, 300, 400, 700]
+LAMBDAS = [100, 200, 300, 400, 700, 1000]
 DIFS = 4  # slots
 SIFS = 1  # slots
-ACK = 2  # slots
+ACK = RTS = CTS = 2  # slots
 FRAME = 100  # slots
 MAX_CW = 1024
 MIN_CW = 4
+LAMBDAINDEX = 0
